5cdf5fe8f8ed929b8c7f52277d5f0ab95efab09e
driton <driton.salihu@tum.de>
2020-06-23 17:42:05 +0200

    Added SSD reconstruction

---
+++ IVC_labs_starting_point/Chapter_5/SSD_rec.m
@@ -0,0 +1,26 @@
+function rec_image = SSD_rec(ref_image, motion_vectors)
+%  Input         : ref_image(Reference Image, YCbCr image)
+%                  motion_vectors
+%
+%  Output        : rec_image (Reconstructed current image, YCbCr image)
+
+    rec_image = zeros(size(ref_image));
+    ref_image = padarray(ref_image, [4, 4], 'both');
+    
+    for x = 1:size(motion_vectors, 1)
+        for y = 1:size(motion_vectors, 2)
+            [mvY, mvX] = ind2sub(8+1, motion_vectors(x, y));
+            
+            ref_XStart = (x-1)*8+mvX;
+            ref_XEnd = x*8+mvX-1;
+            ref_YStart = (y-1)*8+mvY;
+            ref_YEnd = y*8+mvY-1;
+                        
+            rec_image(ref_XStart-mvX+1:ref_XEnd-mvX+1, ref_YStart-mvY+1:ref_YEnd-mvY+1, :) = ...
+                ref_image(ref_XStart:ref_XEnd, ref_YStart:ref_YEnd, :);
+        end 
+    end
+
+end
+
+
--- IVC_labs_starting_point/Chapter_5/test.m
+++ IVC_labs_starting_point/Chapter_5/test.m
@@ -1,10 +1,12 @@
 %% DO NOT MODIFY THIS CODE
-%% This code will call your function and should work without any changes
-% load('image_ycbcr');
-% load('ref_image_ycbcr');
-% clear;
-% clc;
+%% SSD
+clear;
+clc;
+tic
 image = double(imread('lena_small.tif'));
 reference_image = ictRGB2YCbCr(image);
 
 mv_indices = SSD(reference_image(:,:,1), image(:,:,1));
+
+rec_image = SSD_rec(reference_image, mv_indices);
+toc
